1c9fdb51988e738dda770765c76b76221dfdd73a
LesleyPLangdon <61936741+LesleyPLangdon@users.noreply.github.com>
2021-02-23 15:11:06 -0500

    broke into functions and added input validation

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,94 +1,133 @@
 /**
  *
  */
+var comPick;
+var numGames;
+var plrWins = 0;
+var comWins = 0;
+var ties = 0;
+var plrPick;
 function playGame() {
-  var numGames = prompt("How many games would you like to play?");
-  var plrPick;
-  var comPick;
-  var i;
-  var plrWins = 0;
-  var comWins = 0;
-  var ties = 0;
-  for (i = 0; i < numGames; i++) {
-    plrPick = prompt("Please choose rock, paper or scissors").toLowerCase();
-    
-    var rdmPick = Math.floor(Math.random() * 3);
-
-    switch (rdmPick) {
-      case 0:
-        comPick = "rock";
-        break;
-      case 1:
-        comPick = "paper";
-        break;
-      case 2:
-        comPick = "scissors";
+  userChoice();
+  playRound(numGames);
+}
+function userChoice() {
+  numGames = prompt("How many games would you like to play?");
+  return numGames;
+}
+function playRound(numGames) {
+  for (var i = 1; i <= numGames; i++) {
+    console.log("round " + i);
+    if (i>1){
+      score();
     }
-    console.log("round " + (i + 1));
+    
+    plrPick = prompt("Please choose rock, paper or scissors").toLowerCase();
+    checkPlrPick();
+    computerChoice();
+
     console.log(
       "player selected " + plrPick + ", computer selected " + comPick
     );
-    if (plrPick === "rock") {
-      switch (rdmPick) {
-        case 0:
-          result = "tie";
-          ties++;
-          break;
-        case 1:
-          result = "computer wins";
-          comWins++;
-          break;
-        case 2:
-          result = "you win";
-          plrWins++;
-          
+    switch (plrPick) {
+      case "rock":
+        plrRock(comPick);
+        break;
+      case "paper":
+        plrPaper(comPick);
+        break;
+      case "scissors":
+        plrScissors(comPick);
+        break;
       }
-    }
-    else if (plrPick === "paper") {
-      switch (rdmPick) {
-        case 0:
-          result = "you win";
-          plrWins++;
-          break;
-        case 1:
-          result = "tie";
-          ties++;
-          break;
-        case 2:
-          result = "computer wins";
-          comWins++;
-         
-      }
-    }
-    else if (plrPick === "scissors") {
-      switch (rdmPick) {
-        case 0:
-          result = "computer wins";
-          comWins++;
-          break;
-        case 1:
-          result = "you win";
-          plrWins++;
-          break;
-        case 2:
-          result = "tie";
-          ties++;
-      }
-    }
-      else {
-          result = "invalid entry, please try again";
-          i --;
-      }
-    
-    console.log(result);
-    console.log(
-      "score: " +
-        plrWins +
-        " player wins " +
-        comWins +
-        " computer wins " +
-        ties +
-        " ties"
-    );
   }
+  score();
+}
+function checkPlrPick(){
+  /*write function to validate player's choice using enums*/
+  //debugger
+  while(plrPick !== "rock" && plrPick !== "paper" && plrPick !== "scissors") {
+    console.log(plrPick + " is an invalid entry, please try again");
+    plrPick = prompt("Please choose rock, paper or scissors").toLowerCase();
+  }
+  return plrPick;
+}
+function computerChoice() {
+  var rdmPick = Math.floor(Math.random() * 3);
+  switch (rdmPick) {
+    case 0:
+      comPick = "rock";
+      break;
+    case 1:
+      comPick = "paper";
+      break;
+    case 2:
+      comPick = "scissors";
+  }
+  return comPick;
+}
+function plrRock(comPick) {
+  switch (comPick) {
+    case "rock":
+      tie();
+      break;
+    case "paper":
+      cWins();
+      break;
+    case "scissors":
+      pWins();
+      break;
+  }
+}
+function plrPaper(comPick) {
+  switch (comPick) {
+    case "rock":
+      pWins();
+      break;
+    case "paper":
+      tie();
+      break;
+    case "scissors":
+      cWins();
+      break;
+  }
+}
+function plrScissors(comPick) {
+  switch (comPick) {
+    case "rock":
+      cWins();
+      break;
+    case "paper":
+      pWins();
+      break;
+    case "scissors":
+      tie();
+      break;
+  }
+}
+function pWins() {
+  plrWins++;
+  console.log("Result: Player wins");
+  
+}
+function cWins() {
+  comWins++;
+  console.log("Result: Computer wins");
+  
+}
+function tie() {
+  ties++;
+  console.log("Result: Tie");
+  
+}
+function score() {
+  console.log(
+    "score: " +
+      plrWins +
+      " player wins " +
+      comWins +
+      " computer wins " +
+      ties +
+      " ties"
+  );
 }
